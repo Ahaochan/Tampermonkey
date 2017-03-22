b5706952c2ce12fec16f9517f31381bbeee4e91b
Ahaochan <844394093@qq.com>
2017-03-22 13:30:29 +0800

    update imooc download.js add json,xml,raw

--- 慕课网 下载视频.user.js
+++ 慕课网 下载视频.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        慕课网 下载视频
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.1.1
+// @version     0.1.2
 // @description 获取链接，数据来源：http://www.imooc.com/course/ajaxmediainfo/?mid=285&mode=flash。使用方法：进入任意课程点击下载即可。如http://www.imooc.com/learn/285
 // @author      Ahaochan
 // @match       *://*.imooc.com/learn/*
@@ -9,19 +9,30 @@
 // @require     http://code.jquery.com/jquery-1.11.0.min.js
 // ==/UserScript==
 //$(document).ready(function(){
-    //清晰度设置
+    //导出设置
 	var clarityType = 2;
+	var outTextType = "idm";
 	$("div.mod-tab-menu").after(
 		$("<div id='downloadBox' class='course-brief'>"+
-			"<h4 style='font-weight:700;font-size: 16px;marginTop:10px'>下载清晰度 : </h4>"+
-			"<label for='lowClarity'   >Low   </label><input type='radio' id='lowClarity'    name='clarity' value='0' />"+
-			"<label for='middleClarity'>Middle</label><input type='radio' id='middleClarity' name='clarity' value='1' />"+
-			"<label for='heightClarity'>Height</label><input type='radio' id='heightClarity' name='clarity' value='2' checked='checked' /><br/>"+
+			"<div style='float:left;margin-right:70px;'>"+
+				"<h4 style='font-weight:700;font-size: 16px;marginTop:10px'>下载清晰度 : </h4>"+
+				"<label for='lowClarity'   >Low   </label><input type='radio' id='lowClarity'    name='clarity' value='0' />"+
+				"<label for='middleClarity'>Middle</label><input type='radio' id='middleClarity' name='clarity' value='1' />"+
+				"<label for='heightClarity'>Height</label><input type='radio' id='heightClarity' name='clarity' value='2' checked='checked' />"+
+			"</div>"+
+			"<div>"+
+				"<h4 style='font-weight:700;font-size: 16px;marginTop:10px'>导出格式 : </h4>"+
+				"<label for='rawOutText' >raw</label><input type='radio' id='rawOutText'  name='outText' value='raw'/>"+
+				"<label for='idmOutText' >idm </label><input type='radio' id='idmOutText'  name='outText' value='idm' checked='checked' />"+
+				"<label for='xmlOutText' >xml </label><input type='radio' id='xmlOutText'  name='outText' value='xml' />"+
+				"<label for='jsomOutText'>json</label><input type='radio' id='jsomOutText' name='outText' value='json'/><br/>"+
+			"</div>"+
 		"</div>")
 	);
-	$("input:radio[name=clarity]").css("margin","auto 50px auto 3px");//设置单选框
-	$("input:radio[name=clarity]").change(function(){clarityType = this.value;$("#downloadBox textarea").text(getTextLinks());});
-	//清晰度设置
+	$("input:radio").css("margin","auto 50px auto 3px");//设置单选框
+	$("input:radio[name=clarity]").change(function() { clarityType = this.value; $("#downloadBox textarea").text(getTextLinks(clarityType,outTextType)); });
+	$("input:radio[name=outText]").change(function() { outTextType = this.value; $("#downloadBox textarea").text(getTextLinks(clarityType,outTextType)); });
+	//导出设置
 
 	//获取下载链接
 	var videoes = [];
@@ -52,20 +63,33 @@
 			
             //添加全部下载链接
 			if (videoes.length == total) {
-				$("#downloadBox h4:first").after('共' + total + '个视频。已完成解析' + videoes.length + '个视频。<br/>');
-				$("#downloadBox").append($("<textarea style='width:100%;border:2px solid red;padding: 5px;height: 100px;'>"+getTextLinks()+"</textarea>"));//全部链接
+				$("#downloadBox").append('共' + total + '个视频。已完成解析' + videoes.length + '个视频。<br/>');
+				$("#downloadBox").append($("<textarea style='width:100%;border:2px solid red;padding: 5px;height: 100px;'>"+getTextLinks(clarityType,outTextType)+"</textarea>"));//全部链接
+				videoes.sort(function(a,b){
+					if(a.name>b.name)	return 1;
+					else if(a.name<b.name) return -1;
+					else return 0;
+				});
 			}
 		});
 	});
 	//获取下载链接
 
-	function getTextLinks(){
-		var arr = [];
-		for(var i in videoes) {
-			//arr[i] = "filename="+videoes[i].name+"&fileurl="+videoes[i].url[clarityType];
-			arr[i] = videoes[i].url[clarityType];
+	function getTextLinks(clarityType, outTextType){
+		if(outTextType === "json")	return JSON.stringify(videoes);
+		else {
+			var str = "";
+			for(var i in videoes) {
+				if(outTextType === "xml"){
+					str += '\t<video>\n\t\t<url>' + videoes[i].url[clarityType] + '</url>\n\t\t<name>' + videoes[i].name + '</name>\n\t</video>\n';
+				} else if(outTextType === "raw"){
+					str += videoes[i].url[clarityType]+"\n";
+				} else {//idm
+					str += "filename="+videoes[i].name+"&fileurl="+videoes[i].url[clarityType]+"\n";
+				}
+			}
+			if(outTextType === "xml")   str = "<?xml version='1.0' encoding='utf-8' ?>\n<videoes>\n"+str+'</videoes>';
+			return str;
 		}
-		arr.sort();
-		return arr.join("\n");
 	}
 //});
