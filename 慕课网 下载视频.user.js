aaddd2676665c8d9e8bbff3d776cbce97cc2574a
A浩酱 <A>
2017-07-14 22:57:03 +0800

    add download fail tip

--- 慕课网 下载视频.user.js
+++ 慕课网 下载视频.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name        慕课网 下载视频
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.2.5
+// @version     0.2.6
 // @description 获取视频下载链接，使用方法：进入任意课程点击下载即可。如http://www.imooc.com/learn/814。慕课网修复了漏洞，现在只支持v2接口，请打开脚本查看最新视频的下载方法。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @author      Ahaochan
 // @match       http://www.imooc.com/learn/*
@@ -18,151 +18,171 @@
     // 下载地址就是http://v3.mukewang.com/58c65fc3e520e5677f8b457a/H.mp4。
     // 估计是通过http://www.imooc.com/course/14351/medium.m3u8?cdn=aliyun返回的数据，通过某种解密方式获得的神秘代码
     // 思路在这，个人水平不够，修复不了，有能力希望能fork并pull request一下。。。
-    
-	/**--------------------------------获取下载链接---------------------------------------------*/
-	var videoes = [];
-	var $medias = $('.mod-chapters').find('a.J-media-item');
-	var total = $medias.length;
-	var len = total;	
-	//添加提示标签
-	$('.course-menu').append($('<li><a href="javascript:void(0)"><span id="downTip">视频解析中...</span></a></li>'));
-	if(!isLogin){
-		$('#downTip').text('视频下载异常，点击进行登录');
-		$('#downTip').click(function(){
-            var clickEvent  = document.createEvent ('MouseEvents');
-            clickEvent.initEvent ('click', true, true);
-            document.getElementById('js-signin-btn').dispatchEvent (clickEvent);
-		});
-		return;
-	}
-	//遍历获取下载链接
-	$medias.each(function (key, value) {
-		var vid = $(this).parent().attr('data-media-id');
-		var name = $(this).text();
-		var pattern = /\(\d\d:\d\d\)/;
-		if (!pattern.test(name)) {
-			total--;
-			if (key == len - 1 && !total) {
-				$('#downTip').text('无视频下载');
-			}
-			return;
-		}
-		name = name.replace(/\(\d{2}:\d{2}\)/, '').replace(/\s/g, '');
+
+    /**--------------------------------获取下载链接---------------------------------------------*/
+    var videoes = [];
+    var $medias = $('.mod-chapters').find('a.J-media-item');
+    var total = $medias.length;
+    var len = total;
+    //添加提示标签
+    $('.course-menu').append($('<li><a href="javascript:void(0)"><span id="downTip">视频解析中...</span></a></li>'));
+    if (!isLogin) {
+        $('#downTip').text('视频下载异常，点击进行登录')
+            .click(function () {
+                var clickEvent = document.createEvent('MouseEvents');
+                clickEvent.initEvent('click', true, true);
+                document.getElementById('js-signin-btn').dispatchEvent(clickEvent);
+            });
+        return;
+    }
+    //遍历获取下载链接
+    $medias.each(function (key, value) {
+        var vid = $(this).parent().attr('data-media-id');
+        var name = $(this).text();
+        var pattern = /\(\d\d:\d\d\)/;
+        if (!pattern.test(name)) {
+            total--;
+            if (key === len - 1 && !total) {
+                $('#downTip').text('无视频下载');
+            }
+            return;
+        }
+        name = name.replace(/\(\d{2}:\d{2}\)/, '').replace(/\s/g, '');
         v1(vid, name, $(this));
-		//v2(vid, name, $(this));
-		//v3(vid, name, $(this));
-	});
-	/**--------------------------------获取下载链接---------------------------------------------*/
-	/**--------------------------------视频下载解析接口-----------------------------------------*/
+        //v2(vid, name, $(this));
+        //v3(vid, name, $(this));
+    });
+    /**--------------------------------获取下载链接---------------------------------------------*/
+    /**--------------------------------视频下载解析接口-----------------------------------------*/
     /** v1接口，强制转换为v1接口 */
-	function v1(vid, name, item){
-		$.getJSON('/course/ajaxmediainfo/?mid=' + vid + '&mode=flash', function(response) {
-			var url = response.data.result.mpath[0].replace('http://v2', 'http://v1');
-			parseVideo(vid, name, url, item);
-		});
-	}
-	/** v2接口，只能解析v1,v2 */
-	function v2(vid, name, item){
-		$.getJSON('/course/ajaxmediainfo/?mid=' + vid + '&mode=flash', function(response) {
-			var url = response.data.result.mpath[0];
-			parseVideo(vid, name, url, item);
-		});
-	}
-	/** v3接口，解析v1,v2,v3（已废弃） */
-	function v3(vid, name, item){
-		GM_xmlhttpRequest({
-			method: 'GET',
-			url: 'http://m.imooc.com/video/'+vid,
-			onload: function(response) {
-				var pattern = /(http.+mp4)/;
-				var url = response.responseText.match(pattern)[0];
-				parseVideo(vid, name, url, item);
-			}
-		});
-	}
-	/**--------------------------------视频下载解析接口-----------------------------------------*/
-	/**--------------------------------处理数据-------------------------------------------------*/
-	function parseVideo(vid, name, url, item){
-		var urlL = url.replace('H.mp4','M.mp4').replace('M.mp4','L.mp4');
-		var urlM = url.replace('H.mp4','M.mp4').replace('L.mp4','M.mp4');
-		var urlH = url.replace('L.mp4','M.mp4').replace('M.mp4','H.mp4');
-		var video = {
-			vid: vid,
-			name: name,
-			url: [ urlL, urlM, urlH ]
-		};
-		videoes.push(video);
-		//添加单个下载链接
-		var $link = $('<a href="'+video.url[clarityType]+'" style="position:absolute;right:100px;top:0;" target="_blank">下载</a>');
-		$link.bind('DOMNodeInserted', function() {	$(this).find('i').remove();} );//移除子标签
-		item.after($link);
-		//添加全部下载链接
-		if (videoes.length == total) {
-			$('#downTip').text('视频下载(' + total + ')，已复制到剪贴板');
-			videoes.sort(function(a,b){
-				if(a.name>b.name)	return 1;
-				else if(a.name<b.name) return -1;
-				else return 0;
-			});
-			//显示
-			$('#downloadBox').append($('<div style="margin-top:24px;">' +
-				'<div style="border:1px solid #b7bbbf;box-sizing:border-box;border-radius:2px;">' +
-					'<textarea id="downLoadArea" style="width:97%;min-height:100px;padding:8px;color:#555;resize:none;line-height:18px;"></textarea>' +
-				'</div>')
-				);
-			textAreaChange();
-		}
-	}
-	/**--------------------------------处理数据-------------------------------------------------*/
-	
-	/**--------------------------------导出设置-------------------------------------------------*/
-	var clarityType = 2;
-	var outTextType = 'idm';
-	$('div.mod-tab-menu').after(
-		$('<div id="downloadBox" class="course-brief">'+
-			'<div style="float:left;margin-right:30px;">'+
-				'<h4 style="font-weight:700;font-size: 16px;marginTop:10px">下载清晰度 : </h4>'+
-				'<label for="lowClarity"   >普清(L)</label><input type="radio" id="lowClarity"    name="clarity" value="0" />'+
-				'<label for="middleClarity">高清(M)</label><input type="radio" id="middleClarity" name="clarity" value="1" />'+
-				'<label for="highClarity"  >超清(H)</label><input type="radio" id="highClarity"   name="clarity" value="2" checked="checked" />'+
-			'</div>'+
-			'<div>'+
-				'<h4 style="font-weight:700;font-size: 16px;marginTop:10px">导出格式 : </h4>'+
-				'<label for="rawOutText" >raw </label><input type="radio" id="rawOutText"  name="outText" value="raw"/>'+
-				'<label for="idmOutText" >idm </label><input type="radio" id="idmOutText"  name="outText" value="idm" checked="checked" />'+
-				'<label for="xmlOutText" >xml </label><input type="radio" id="xmlOutText"  name="outText" value="xml" />'+
-				'<label for="jsomOutText">json</label><input type="radio" id="jsomOutText" name="outText" value="json"/><br/>'+
-			'</div>'+
-		'</div>')
-	);
-	$('input:radio').css('margin','auto 50px auto 3px');//设置单选框
-	$('input:radio[name=clarity]').change(function() {  clarityType = this.value; 	textAreaChange();	});
-	$('input:radio[name=outText]').change(function() {	outTextType = this.value;	textAreaChange();	});
-	function textAreaChange(){
-		var downloadTextArea = getTextLinks(clarityType,outTextType);
-		GM_setClipboard(downloadTextArea);
-		$('#downloadBox textarea').text(downloadTextArea);
-	}
-	/**--------------------------------导出设置-------------------------------------------------*/
-	
-	
-	/**--------------------------------格式化下载链接用以显示---------------------------------*/
-	function getTextLinks(clarityType, outTextType){
-		if(outTextType === 'json')	return JSON.stringify(videoes);
-		else {
-			var str = '';
-			for(var i in videoes) {
-				if(outTextType === 'xml'){
-					str += '\t<video>\n\t\t<url>' + videoes[i].url[clarityType] + '</url>\n\t\t<name>' + videoes[i].name + '</name>\n\t</video>\n';
-				} else if(outTextType === 'raw'){
-					str += videoes[i].url[clarityType]+'\n';
-				} else {//idm
-					str += 'filename='+videoes[i].name+'&fileurl='+videoes[i].url[clarityType]+'\n';
-				}
-			}
-			if(outTextType === 'xml')   str = '<?xml version="1.0" encoding="utf-8" ?>\n<videoes>\n'+str+'</videoes>';
-			return str;
-		}
-	}
-	/**--------------------------------格式化下载链接用以显示---------------------------------*/
+    function v1(vid, name, item) {
+        $.getJSON('/course/ajaxmediainfo/?mid=' + vid + '&mode=flash', function (response) {
+            console.log('加载数据:' + vid);
+            if (response.data.result.mpath instanceof Array) {
+                var url = response.data.result.mpath[0].replace('http://v2', 'http://v1');
+                parseVideo(vid, name, url, item);
+            } else {
+                $('#downTip').text('不支持新版视频, 若要下载请查看源码中的注释');
+            }
+
+        });
+    }
+
+    /** v2接口，只能解析v1,v2（已废弃） */
+    function v2(vid, name, item) {
+        $.getJSON('/course/ajaxmediainfo/?mid=' + vid + '&mode=flash', function (response) {
+            var url = response.data.result.mpath[0];
+            parseVideo(vid, name, url, item);
+        });
+    }
+
+    /** v3接口，解析v1,v2,v3（已废弃） */
+    function v3(vid, name, item) {
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'http://m.imooc.com/video/' + vid,
+            onload: function (response) {
+                var pattern = /(http.+mp4)/;
+                var url = response.responseText.match(pattern)[0];
+                parseVideo(vid, name, url, item);
+            }
+        });
+    }
+
+    /**--------------------------------视频下载解析接口-----------------------------------------*/
+    /**--------------------------------处理数据-------------------------------------------------*/
+    function parseVideo(vid, name, url, item) {
+        var urlL = url.replace('H.mp4', 'M.mp4').replace('M.mp4', 'L.mp4');
+        var urlM = url.replace('H.mp4', 'M.mp4').replace('L.mp4', 'M.mp4');
+        var urlH = url.replace('L.mp4', 'M.mp4').replace('M.mp4', 'H.mp4');
+        var video = {
+            vid: vid,
+            name: name,
+            url: [urlL, urlM, urlH]
+        };
+        videoes.push(video);
+        //添加单个下载链接
+        var $link = $('<a href="' + video.url[clarityType] + '" style="position:absolute;right:100px;top:0;" target="_blank">下载</a>');
+        $link.bind('DOMNodeInserted', function () {
+            $(this).find('i').remove();
+        });//移除子标签
+        item.after($link);
+        //添加全部下载链接
+        if (videoes.length === total) {
+            $('#downTip').text('视频下载(' + total + ')，已复制到剪贴板');
+            videoes.sort(function (a, b) {
+                if (a.name > b.name)    return 1;
+                else if (a.name < b.name) return -1;
+                else return 0;
+            });
+            //显示
+            $('#downloadBox').append($('<div style="margin-top:24px;">' +
+                '<div style="border:1px solid #b7bbbf;box-sizing:border-box;border-radius:2px;">' +
+                '<textarea id="downLoadArea" style="width:97%;min-height:100px;padding:8px;color:#555;resize:none;line-height:18px;"></textarea>' +
+                '</div>')
+            );
+            textAreaChange();
+        }
+    }
+
+    /**--------------------------------处理数据-------------------------------------------------*/
+
+    /**--------------------------------导出设置-------------------------------------------------*/
+    var clarityType = 2;
+    var outTextType = 'idm';
+    $('div.mod-tab-menu').after(
+        $('<div id="downloadBox" class="course-brief">' +
+            '<div style="float:left;margin-right:30px;">' +
+            '<h4 style="font-weight:700;font-size: 16px;marginTop:10px">下载清晰度 : </h4>' +
+            '<label for="lowClarity"   >普清(L)</label><input type="radio" id="lowClarity"    name="clarity" value="0" />' +
+            '<label for="middleClarity">高清(M)</label><input type="radio" id="middleClarity" name="clarity" value="1" />' +
+            '<label for="highClarity"  >超清(H)</label><input type="radio" id="highClarity"   name="clarity" value="2" checked="checked" />' +
+            '</div>' +
+            '<div>' +
+            '<h4 style="font-weight:700;font-size: 16px;marginTop:10px">导出格式 : </h4>' +
+            '<label for="rawOutText" >raw </label><input type="radio" id="rawOutText"  name="outText" value="raw"/>' +
+            '<label for="idmOutText" >idm </label><input type="radio" id="idmOutText"  name="outText" value="idm" checked="checked" />' +
+            '<label for="xmlOutText" >xml </label><input type="radio" id="xmlOutText"  name="outText" value="xml" />' +
+            '<label for="jsomOutText">json</label><input type="radio" id="jsomOutText" name="outText" value="json"/><br/>' +
+            '</div>' +
+            '</div>')
+    );
+    $('input:radio').css('margin', 'auto 50px auto 3px');//设置单选框
+    $('input:radio[name=clarity]').change(function () {
+        clarityType = this.value;
+        textAreaChange();
+    });
+    $('input:radio[name=outText]').change(function () {
+        outTextType = this.value;
+        textAreaChange();
+    });
+    function textAreaChange() {
+        var downloadTextArea = getTextLinks(clarityType, outTextType);
+        GM_setClipboard(downloadTextArea);
+        $('#downloadBox').find('textarea').text(downloadTextArea);
+    }
+
+    /**--------------------------------导出设置-------------------------------------------------*/
+
+
+    /**--------------------------------格式化下载链接用以显示---------------------------------*/
+    function getTextLinks(clarityType, outTextType) {
+        if (outTextType === 'json')    return JSON.stringify(videoes);
+        else {
+            var str = '';
+            for (var i in videoes) {
+                if (outTextType === 'xml') {
+                    str += '\t<video>\n\t\t<url>' + videoes[i].url[clarityType] + '</url>\n\t\t<name>' + videoes[i].name + '</name>\n\t</video>\n';
+                } else if (outTextType === 'raw') {
+                    str += videoes[i].url[clarityType] + '\n';
+                } else {//idm
+                    str += 'filename=' + videoes[i].name + '&fileurl=' + videoes[i].url[clarityType] + '\n';
+                }
+            }
+            if (outTextType === 'xml') str = '<?xml version="1.0" encoding="utf-8" ?>\n<videoes>\n' + str + '</videoes>';
+            return str;
+        }
+    }
+
+    /**--------------------------------格式化下载链接用以显示---------------------------------*/
 })();
--- 慕课网 找回路径课程.user.js
+++ 慕课网 找回路径课程.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name        慕课网 找回路径课程
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.1.4
+// @version     0.1.5
 // @description 将慕课网消失的路径课程显示出来，数据来源：慕课网App4.2.3。使用方法：点击首页上方职业路径，或者输入http://class.imooc.com。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @author      Ahaochan
 // @match       http://class.imooc.com*
@@ -11,117 +11,118 @@
 
 (function () {
     'use strict';
-	var itemTitles = new Array(
-		/*  0: */"",
-		/*  1: */"",
-		/*  2: */"",
-		/*  3: */"Web前端工程师成长第一阶段(基础篇)",
-		/*  4: */"",
-		/*  5: */"",
-		/*  6: */"",
-		/*  7: */"",
-		/*  8: */"",
-		/*  9: */"",
-		/* 10: */"",
-		/* 11: */"PHP开发工程师闯关记--初识PHP",
-		/* 12: */"",
-		/* 13: */"",
-		/* 14: */"",
-		/* 15: */"",
-		/* 16: */"",
-		/* 17: */"从0开始学习制作QQ侧滑菜单",
-		/* 18: */"模式宗师养成宝典之Java版",
-		/* 19: */"",
-		/* 20: */"jQuery源码探索之旅",
-		/* 21: */"电商网站全站开发攻略",
-		/* 22: */"响应式布局那些事",
-		/* 23: */"搞定Java加解密",
-		/* 24: */"Android加薪利器--自定义View",
-		/* 25: */"",
-		/* 26: */"前端经典案例集萃之“图片、信息展示”",
-		/* 27: */"从零开始学习ThinkPHP框架",
-		/* 28: */"高德开发者必由之路——JS API篇",
-		/* 29: */"高德开发者必由之路——Android SDK篇",
-		/* 30: */"",
-		/* 31: */"Java工程师",
-		/* 32: */"Web前端工程师",
-		/* 33: */"Android工程师",
-		/* 34: */"PHP工程师",
-		/* 35: */"前端经典案例集萃之 \"网页常用特效\"",
-		/* 36: */"Android加薪利器——断点续传",
-		/* 37: */"C语言学习攻略",
-		/* 38: */"Tony老师聊shell",
-		/* 39: */"Swift加薪利器-iOS动画特辑",
-		/* 40: */"Oracle数据库开发必备利器",
-		/* 41: */"",
-		/* 42: */"C++远征攻略",
-		/* 43: */"教你HTML5开发爱心鱼游戏",
-		/* 44: */"小慕感恩计划-实战Hot!!!",
-		/* 45: */"Linux运维工程师",
-		/* 46: */"iOS苹果表开发攻略",
-		/* 47: */"Cocos2d-x游戏开发快速入门",
-		/* 48: */"Hibernate开发宝典",
-		/* 49: */"Linux shell运维实战",
-		/* 50: */"Android-微信热门功能合集",
-		/* 51: */"搞定python基础",
-		/* 52: */"玩嗨Python进阶",
-		/* 53: */"PHP微信公众平台开发攻略",
-		/* 54: */"快速搞定PHP第三方登录",
-		/* 55: */"带你玩转Yii框架",
-		/* 56: */"探索Python世界",
-		/* 57: */"Android必备技能之基础组件",
-		/* 58: */"安卓特效合集豪华套餐",
-		/* 59: */"搞定Java SSM框架开发",
-		/* 60: */"SSH框架探幽"
-		);
-	var course = {
-		"route"    : { "name" : "路线", "id" : [45,34,33,32,31] },
-		"all"      : { "name" : "全部", "id" : [3,11,17,18,20,21,22,23,24,26,27,28,29,31,32,33,34,35,36,37,38,39,40,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60] },
-		"web_font" : { "name" : "前端", "id" : [43,35,28,26,22,20,3] },
-		"web_back" : { "name" : "后端", "id" : [60,59,56,55,54,53,52,51,49,48,42,40,37,38,23,18,27,11] },
-		"mobile"   : { "name" : "移动", "id" : [58,57,50,47,46,44,39,36,29,24,17] },
-		"station"  : { "name" : "整站", "id" : [21] }
+    var itemTitles = ["",
+        /*  1: */"",
+        /*  2: */"",
+        /*  3: */"Web前端工程师成长第一阶段(基础篇)",
+        /*  4: */"",
+        /*  5: */"",
+        /*  6: */"",
+        /*  7: */"",
+        /*  8: */"",
+        /*  9: */"",
+        /* 10: */"",
+        /* 11: */"PHP开发工程师闯关记--初识PHP",
+        /* 12: */"",
+        /* 13: */"",
+        /* 14: */"",
+        /* 15: */"",
+        /* 16: */"",
+        /* 17: */"从0开始学习制作QQ侧滑菜单",
+        /* 18: */"模式宗师养成宝典之Java版",
+        /* 19: */"",
+        /* 20: */"jQuery源码探索之旅",
+        /* 21: */"电商网站全站开发攻略",
+        /* 22: */"响应式布局那些事",
+        /* 23: */"搞定Java加解密",
+        /* 24: */"Android加薪利器--自定义View",
+        /* 25: */"",
+        /* 26: */"前端经典案例集萃之“图片、信息展示”",
+        /* 27: */"从零开始学习ThinkPHP框架",
+        /* 28: */"高德开发者必由之路——JS API篇",
+        /* 29: */"高德开发者必由之路——Android SDK篇",
+        /* 30: */"",
+        /* 31: */"Java工程师",
+        /* 32: */"Web前端工程师",
+        /* 33: */"Android工程师",
+        /* 34: */"PHP工程师",
+        /* 35: */"前端经典案例集萃之 \"网页常用特效\"",
+        /* 36: */"Android加薪利器——断点续传",
+        /* 37: */"C语言学习攻略",
+        /* 38: */"Tony老师聊shell",
+        /* 39: */"Swift加薪利器-iOS动画特辑",
+        /* 40: */"Oracle数据库开发必备利器",
+        /* 41: */"",
+        /* 42: */"C++远征攻略",
+        /* 43: */"教你HTML5开发爱心鱼游戏",
+        /* 44: */"小慕感恩计划-实战Hot!!!",
+        /* 45: */"Linux运维工程师",
+        /* 46: */"iOS苹果表开发攻略",
+        /* 47: */"Cocos2d-x游戏开发快速入门",
+        /* 48: */"Hibernate开发宝典",
+        /* 49: */"Linux shell运维实战",
+        /* 50: */"Android-微信热门功能合集",
+        /* 51: */"搞定python基础",
+        /* 52: */"玩嗨Python进阶",
+        /* 53: */"PHP微信公众平台开发攻略",
+        /* 54: */"快速搞定PHP第三方登录",
+        /* 55: */"带你玩转Yii框架",
+        /* 56: */"探索Python世界",
+        /* 57: */"Android必备技能之基础组件",
+        /* 58: */"安卓特效合集豪华套餐",
+        /* 59: */"搞定Java SSM框架开发",
+        /* 60: */"SSH框架探幽"];
+    var course = {
+        "route": {"name": "路线", "id": [45, 34, 33, 32, 31]},
+        "all": {
+            "name": "全部",
+            "id": [3, 11, 17, 18, 20, 21, 22, 23, 24, 26, 27, 28, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+        },
+        "web_font": {"name": "前端", "id": [43, 35, 28, 26, 22, 20, 3]},
+        "web_back": {"name": "后端", "id": [60, 59, 56, 55, 54, 53, 52, 51, 49, 48, 42, 40, 37, 38, 23, 18, 27, 11]},
+        "mobile": {"name": "移动", "id": [58, 57, 50, 47, 46, 44, 39, 36, 29, 24, 17]},
+        "station": {"name": "整站", "id": [21]}
     };
 
-	//创建存储路径课程的内容div
+    //创建存储路径课程的内容div
     var $box = $('<div class="program-list-wrap clearfix"></div>');
     $('.program-list').prepend($box);
-    
+
     //获取图片数组
     var imgs = [];
-    $('.program-list-head div').each(function(){
-        imgs.push($(this).css('background-image').replace('"',''));
+    $('.program-list-head div').each(function () {
+        imgs.push($(this).css('background-image').replace('"', ''));
     });
-    
+
     //创建导航div
-	$('.tab-nav a')
+    $('.tab-nav a')
         .removeAttr('href')
         .removeAttr('data-type')//移除原本的点击事件依赖的属性
         .find("span")
-        .each(function(i){{//获取a标签中的span标签，更改标题名
+        .each(function (i) {//获取a标签中的span标签，更改标题名
             var key = Object.keys(course)[i];//获取course的第i个属性名
             $(this).text(course[key].name).parent()
                 .unbind('click')
-                .click(function freshBox(){//重新绑定点击事件
-                            $box.empty();
-                            for(var i in course[key].id){
-                                var pid = course[key].id[i];
-                                var $item = $('<a class="program-item" href="http://www.imooc.com/course/programdetail/pid/'+pid+'" target="_blank">'+
-                                                    '<div class="shadow">'+
-                                                        '<div class="program-list-head">'+
-                                                            '<div class="" style="background-image:'+imgs[parseInt(Math.random()*imgs.length)]+';"></div>'+
-                                                        '</div>'+
-                                                        '<div class="program-list-cont">'+
-                                                            '<div class="program-list-tit">'+itemTitles[pid]+'</div>'+
-                                                        '</div>'+
-                                                    '</div>'+
-                                                    '<div class="c-line"></div>'+
-                                                    '<div class="d-line"></div>'+
-                                                '</a>');
-                                $box.append($item);
-                            }
+                .click(function freshBox() {//重新绑定点击事件
+                        $box.empty();
+                        for (var i in course[key].id) {
+                            var pid = course[key].id[i];
+                            var $item = $('<a class="program-item" href="http://www.imooc.com/course/programdetail/pid/' + pid + '" target="_blank">' +
+                                '<div class="shadow">' +
+                                '<div class="program-list-head">' +
+                                '<div class="" style="background-image:' + imgs[parseInt(Math.random() * imgs.length)] + ';"></div>' +
+                                '</div>' +
+                                '<div class="program-list-cont">' +
+                                '<div class="program-list-tit">' + itemTitles[pid] + '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '<div class="c-line"></div>' +
+                                '<div class="d-line"></div>' +
+                                '</a>');
+                            $box.append($item);
                         }
-                    );
-        });
-    $('.tab-nav a').first().click();
+                    }
+                );
+        })
+        .first().click();
 })();
