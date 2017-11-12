c506c1e8611416903dc71968f525cb882300b243
Ahaochan <844394093@qq.com>
2017-11-13 00:54:09 +0800

    更新匹配url

--- Greasy Fork 替换大图.user.js
+++ Greasy Fork 替换大图.user.js
@@ -1,18 +1,15 @@
 // ==UserScript==
-// @name        Greasy Fork 替换原图
+// @name:zh     Greasy Fork 替换原图
 // @name:ja 	Greasy Fork replace original image
 // @name:en  	Greasy Fork replace original image
 // @namespace   https://github.com/Ahaochan/Tampermonkey
 // @version     0.0.1
-// @description 脚本详情页预览图替换为原图, 方便拖拽保存。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
+// @description:zh 脚本详情页预览图替换为原图, 方便拖拽保存。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @description:ja Replacement script details page preview is the original image, easy to drag and save. Github:https://github.com/Ahaochan/Tampermonkey. Star and fork is welcome.
 // @description:en Replacement script details page preview is the original image, easy to drag and save. Github:https://github.com/Ahaochan/Tampermonkey. Star and fork is welcome.
 // @author      Ahaochan
 // @supportURL  https://github.com/Ahaochan/Tampermonkey
-// @match       http://greasyfork.org*
-// @match       https://greasyfork.org*
-// @match       http://greasyfork.org/*
-// @match       https://greasyfork.org/*
+// @include     *://greasyfork.org*
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // ==/UserScript==
 
