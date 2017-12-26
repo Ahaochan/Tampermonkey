3faa0da28939ee2774dc275f3439cbbc1e49e63d
Ahaochan <844394093@qq.com>
2017-12-26 15:19:19 +0800

    兼容https和http的url路径

--- 慕课网 找回路径课程.user.js
+++ 慕课网 找回路径课程.user.js
@@ -1,19 +1,17 @@
 ﻿// ==UserScript==
 // @name        慕课网 找回路径课程
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.1.8
+// @version     0.1.9
 // @description 将慕课网消失的路径课程显示出来，数据来源：慕课网App4.2.3。使用方法：点击首页上方职业路径，或者输入http://class.imooc.com。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @author      Ahaochan
-// @match       http://www.imooc.com/*
-// @match       https://www.imooc.com/*
-// @match       http://class.imooc.com/*
+// @include     http*://*.imooc.com*
 // @match       https://class.imooc.com/*
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // ==/UserScript==
 
 (function ($) {
     'use strict';
-    if (location.href.indexOf('http://class.imooc.com') === -1) {
+    if (location.href.indexOf('class.imooc.com') === -1) {
         return;
     }
 
