07b737f5bcf59ade05de18f01e91f912b18cf777
Ahaochan <844394093@qq.com>
2018-06-10 11:43:21 +0800

    更新注释

--- fuck天猫.js
+++ fuck天猫.js
@@ -2,7 +2,7 @@
 // @name        fuck天猫
 // @namespace   https://github.com/Ahaochan/Tampermonkey
 // @version     0.0.1
-// @description 屏蔽淘宝下的天猫搜索结果, 还我一个干净的淘宝。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
+// @description 天猫没有差评, 所以开发出这个脚本, 屏蔽淘宝下的天猫搜索结果, 还我一个干净的淘宝。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @author      Ahaochan
 // @include     http*://*.taobao.com*
 // @match       https://s.taobao.com
