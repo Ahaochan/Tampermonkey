c5a4ad3ad5b6d272b046e67b9be9b541fd4fed3f
Ahaochan <844394093@qq.com>
2018-11-29 11:13:26 +0800

    1. 谷歌翻译页面改版 2. 修复<br/>被替换的bug

--- Google翻译 替换.user.js
+++ Google翻译 替换.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name        Google Translate replace
-// @name:zh-CN  Google翻译 替换
-// @name:zh-TW  Google翻譯 替換
+// @name:zh     Google翻译 替换
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.0.2
+// @version     0.0.3
 // @description Replace the translated string. Github:https://github.com/Ahaochan/Tampermonkey. Star and fork is welcome.
-// @description:zh-CN 对翻译后的字符串进行替换。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
-// @description:zh-TW 對翻譯後的字符串進行替換。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
+// @description:zh 对翻译后的字符串进行替换。github:https://github.com/Ahaochan/Tampermonkey，欢迎star和fork。
 // @author      Ahaochan
 // @include     http*://translate.google.*
 // @grant       GM.setValue
@@ -14,29 +12,30 @@
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // ==/UserScript==
 
-$(function () {
+jQuery(function ($) {
     'use strict';
 
     // 1. 初始化之前设置的 pattern 和 replace
-    var $div = $('<div class="gt-lang-sugg-message goog-inline-block je"' +
-        ' style="float: right; padding: 2px">' +
-        '<input id="ahao-pattern" placeholder="pattern" style="width: 50px;"> >>' +
-        '<input id="ahao-replace" placeholder="replace" style="width: 50px; margin: 0 4px;">' +
-        '<input id="ahao-button" type="button" class="jfk-button jfk-button-action" value="replace">' +
-        '</div>');
-    $('#gt-lang-right').append($div);
+    let $div = $('<div class="tlid-input-button input-button header-button tlid-input-button-docs documents-icon" role="tab" tabindex="-1">' +
+        '<div class="text">' +
+        '   <input id="ahao-pattern" placeholder="pattern" style="width: 50px;"> >>' +
+        '   <input id="ahao-replace" placeholder="replace" style="width: 50px; margin: 0 4px;">' +
+        '   <span id="ahao-button">替换</span>' +
+        '</div></div>');
+    $('.tlid-input-button-container').append($div);
     GM.getValue('pattern').then(function (value) { $('#ahao-pattern').val(value); });
     GM.getValue('replace').then(function (value) { $('#ahao-replace').val(value); });
 
     // 2. 绑定替换事件
-    var $resultBox = $('#result_box');
     $('#ahao-button').on('click', function () {
-        var pattern = $('#ahao-pattern').val();
-        var replace = $('#ahao-replace').val();
+        let pattern = $('#ahao-pattern').val();
+        let replace = $('#ahao-replace').val();
         GM.setValue('pattern', pattern);
         GM.setValue('replace', replace);
 
-        var text = $resultBox.text();
-        $resultBox.text(text.replace(new RegExp(pattern, 'gm'), replace));
+        $('.translation').find('span').each(function () {
+            let $this = $(this);
+            $this.text($this.text().replace(new RegExp(pattern, 'gm'), replace));
+        });
     });
 });
