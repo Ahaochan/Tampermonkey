e49dbed23445affbce8820b77d3f40039e9532c1
Ahaochan <844394093@qq.com>
2020-08-05 09:23:01 +0800

    fix bug 等待按钮出现再点击

--- 东哥是我好兄弟.user.js
+++ 东哥是我好兄弟.user.js
@@ -105,7 +105,7 @@ jQuery(function ($) {
     });
 
     // 京东图书【https://pro.m.jd.com/mall/active/3SC6rw5iBg66qrXPGmZMqFDwcyXi/index.html 】
-    exec(/pro(dev)?\.m\.jd\.com\/mall\/active\/.*\/index\.html/, '.signIn_module, span.chance-cn-num', () => {
+    exec(/pro(dev)?\.m\.jd\.com\/mall\/active\/.*\/index\.html/, '.signIn_module, span.chance-cn-num, .sign_btn', () => {
         $('.signIn_btn').click();
         $('.signIn_bg').click();
         $('.sign_btn').click();
