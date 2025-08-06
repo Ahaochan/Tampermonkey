2209987d8b03fb1abf027273531e2559b23918d4
Ahaochan <844394093@qq.com>
2025-08-06 13:47:14 +0800

    更新CDN

--- Pixiv 增强.user.js
+++ Pixiv 增强.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN  Pixiv 增强
 // @name:zh-TW  Pixiv 增強
 // @namespace   https://github.com/Ahaochan/Tampermonkey
-// @version     0.9.2
+// @version     0.9.3
 // @icon        https://www.pixiv.net/favicon.ico
 // @description Focus on immersive experience, 1. Block ads, directly access popular pictures 2. Use user to enter the way to search 3. Search pid , uid and author 4. Display original image and size, picture rename, download original image | gif map | Zip|multiple map zip 5. display artist id, artist background image 6. auto load comment 7. dynamic markup work type 8. remove redirection 9. single page sort 10. control panel select desired function github: https://github.com/Ahaochan/Tampermonkey, welcome to star and fork.
 // @description:ja    没入型体験に焦点を当てる、1.人気の写真に直接アクセスする広告をブロックする2.検索する方法を入力するためにユーザーを使用する3.検索pid uid と創作家 4.元の画像とサイズを表示する Zip | multiple map zip 5.アーティストID、アーティストの背景画像を表示します。6.自動ロードコメントを追加します。7.動的マークアップ作業タイプを指定します。8.リダイレクトを削除します。9.シングルページソート10.コントロールパネルを選択します。github：https://github.com/Ahaochan/Tampermonkey、スターとフォークへようこそ。
@@ -29,9 +29,9 @@
 // @grant       GM_setClipboard
 // @grant       GM_setValue
 // @grant       GM_getValue
-// @require     https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/2.2.4/jquery.min.js
-// @require     https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/jszip/3.1.4/jszip.min.js
-// @require     https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/FileSaver.js/1.3.2/FileSaver.min.js
+// @require     https://update.greasyfork.org/scripts/505351/1435420/jquery%20221.js
+// @require     https://update.greasyfork.org/scripts/518632/1489865/jszip-min-js.js
+// @require     https://update.greasyfork.org/scripts/498746/1399668/FileSaver.js
 // @require     https://greasyfork.org/scripts/2963-gif-js/code/gifjs.js?version=8596
 // @require     https://greasyfork.org/scripts/375359-gm4-polyfill-1-0-1/code/gm4-polyfill-101.js?version=652238
 // @run-at      document-end
